1afa6a354ba2c3c976d65e6925fa5d69120fcacd
Akash Das <akashakki1100@gmail.com>
2025-12-01 15:41:10 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Navigation from "./Navigation";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <BrowserRouter>
+        <Navigation/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   )
 }
